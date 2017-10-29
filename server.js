c6679828e55a1421d283b00d94d571b473baae2e
tutstu <ashurenu1993@gmail.com>
2017-10-30 01:12:27 +0530

    database error

--- server.js
+++ server.js
@@ -6,18 +6,48 @@ var morgan=require('morgan');
 var app=express();
 var http=require('http').Server(app);
 
-var connection = mysql.createConnection({
-					  host     : 'us-cdbr-iron-east-05.cleardb.net',
+// var connection = mysql.createConnection({
+// 					  host     : 'us-cdbr-iron-east-05.cleardb.net',
+// 					  user     : 'b34a8e07148a7d',
+// 					  password : 'b9c7775c',
+// 					  database : 'heroku_02dc774aa58a4ec',
+// 					  multipleStatements: true
+// 					});
+ 
+// connection.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected to database");
+// });
+
+
+function handleDisconnect() {
+  connection = mysql.createConnection({host: 'us-cdbr-iron-east-05.cleardb.net',
 					  user     : 'b34a8e07148a7d',
 					  password : 'b9c7775c',
 					  database : 'heroku_02dc774aa58a4ec',
-					  multipleStatements: true
-					});
- 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected to database");
-});
+					  multipleStatements: true}); 
+                                                  
+
+  connection.connect(function(err) {              
+    if(err) {                                     
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); 
+    }                                     
+  });                                     
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+      handleDisconnect();                         
+    } else {                                      
+      throw err;                                  
+    }
+  });
+}
+
+handleDisconnect();
+
+
+
 
 
 app.use(bodyParser.urlencoded({extended:true}));
