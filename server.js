786e15b38ad219af4edcf4a1453028eb4c3b6f7b
Ashutosh <ashurenu1993@gmail.com>
2018-04-19 15:13:02 +0530

    two lines added

--- server.js
+++ server.js
@@ -20,31 +20,9 @@ connection.connect(function(err) {
 });
 
 
-// function handleDisconnect() {
-//   connection = mysql.createConnection({host: '127.0.0.1',
-// 					  user     : 'root',
-// 					  password : '',
-// 					  database : 'mindler',
-// 					  multipleStatements: true}); 
-                                                  
+added First line
+added second line
 
-//   connection.connect(function(err) {              
-//     if(err) {                                     
-//      console.log('error when connecting to db:', err);
-//       setTimeout(handleDisconnect, 2000); 
-//     }                                     
-//   });                                     
-//   connection.on('error', function(err) {
-//     console.log('db error', err);
-//     if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
-//       handleDisconnect();                         
-//     } else {                                      
-//       throw err;                                  
-//     }
-//   });
-// }
-
-// handleDisconnect();
 
 
 
